c92fd60c30b500f97246430d10a74305cd950aa3
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-01-22 19:51:43 +0200

    Adding task 2,3,4

---
+++ 01. Stacks and Queues/Task 03.DecimalToBinaryConverter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03.DecimalToBinaryConverter
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            var decimalNumber = int.Parse(Console.ReadLine());
+            var stack = new Stack<int>();
+            if (decimalNumber == 0)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+            while (decimalNumber != 0)
+            {
+                stack.Push(decimalNumber % 2);
+                decimalNumber /= 2;
+            }
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 01. Stacks and Queues/Task 04.MatchingBrackets/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_04.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO : 
+
+            /*   var input = Console.ReadLine();
+               var stack = new Stack<int>();
+               //TODO: Read input from console
+               for (int i = 0; i < input.Length; i++)
+               {
+                   char ch = input[i];
+                   if (ch == '(')
+                   {
+                       stack.Push(i);
+                   }
+                   else if (ch == ')')
+                   {
+                       int startIndex = stack.Pop();
+                       string contents = input.Substring(startIndex, i – startIndex + 1);
+                       Console.WriteLine(contents);
+                   }    
+                 }
+               */
+        }
+
+    }
+
+}
