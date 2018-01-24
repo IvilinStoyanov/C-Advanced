d5f9f4f98b0f44d5f5f10ccf17819e648961f6a8
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-01-24 15:34:35 +0200

    Adding Stacks and Queues - LAB

---
+++ 01. Stacks and Queues - LAB/Task 05.HotPotato/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_05.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            var toss = int.Parse(Console.ReadLine());
+            var childrens = new Queue<string>(input);
+            while (childrens.Count != 1)
+            {
+                for (int i = 1; i < toss; i++)
+                {
+                    childrens.Enqueue(childrens.Dequeue());
+                }
+                Console.WriteLine($"Removed {childrens.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {childrens.Dequeue()}");        
+        }      
+    }
+}
