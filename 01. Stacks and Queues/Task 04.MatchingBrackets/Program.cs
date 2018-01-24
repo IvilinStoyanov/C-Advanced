eb240df453632fa0b773107dc02326538d317169
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-01-24 15:33:41 +0200

    Adding Tasks - 4, 5, 6

--- 01. Stacks and Queues/Task 04.MatchingBrackets/Program.cs
+++ 01. Stacks and Queues/Task 04.MatchingBrackets/Program.cs
@@ -10,28 +10,23 @@ namespace Task_04.MatchingBrackets
     {
         static void Main(string[] args)
         {
-            // TODO : 
-
-            /*   var input = Console.ReadLine();
-               var stack = new Stack<int>();
-               //TODO: Read input from console
-               for (int i = 0; i < input.Length; i++)
-               {
-                   char ch = input[i];
-                   if (ch == '(')
-                   {
-                       stack.Push(i);
-                   }
-                   else if (ch == ')')
-                   {
-                       int startIndex = stack.Pop();
-                       string contents = input.Substring(startIndex, i – startIndex + 1);
-                       Console.WriteLine(contents);
-                   }    
-                 }
-               */
+            var input = Console.ReadLine();
+            var stack = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char ch = input[i];
+                if (ch == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (ch == ')')
+                {
+                    int startIndex = stack.Pop();
+                    string contents = input.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(contents);
+                }
+            }
         }
-
     }
 
 }
