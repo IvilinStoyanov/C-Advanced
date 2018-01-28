3839bf4189047f9e5e165cf1e2225a9a00fbb084
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-01-28 18:38:15 +0200

    Adding BashSoft project till Part 2(Problem 03)

---
+++ BashSoft/BashSoft/ExceptionMessages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    class ExceptionMessages
+    {
+        public const string ExampleExceptionMessages = "Example message";
+
+        public const string  InitializingData = "Data is already initialized"; 
+    }
+}
---
+++ BashSoft/BashSoft/StudentsRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    public static class StudentsRepository
+    {
+        public static bool isDataInitialized = false;
+        private static Dictionary<string, Dictionary<string, List<int>>> studentsByCourse =
+            new Dictionary<string, Dictionary<string, List<int>>>();
+
+        public static void InitializeData()
+        {
+            if(!isDataInitialized)
+            {
+                OutputWriter.WriteMessageOnNewLine("Reading data...");
+                studentsByCourse = new Dictionary<string, Dictionary<string, List<int>>>();
+                ReadData();            
+            }
+            else
+            {
+                OutputWriter.WriteMessageOnNewLine(ExceptionMessages.InitializingData);
+            }
+        }
+
+        private static void ReadData()
+        {
+            string input = Console.ReadLine();
+
+            while(!string.IsNullOrEmpty(input))
+            {
+                string[] token = input.Split(' ');
+                string course = token[0];
+                string student = token[1];
+                int mark = int.Parse(token[2]);
+
+                if (!studentsByCourse.ContainsKey(course))
+                {
+                    studentsByCourse.Add(course, new Dictionary<string, List<int>>());
+                }
+                if(!studentsByCourse[course].ContainsKey(student))
+                {
+                    studentsByCourse[course].Add(student, new List<int>());
+                }
+                studentsByCourse[course][student].Add(mark);
+                input = Console.ReadLine();
+            }
+            isDataInitialized = true;
+            OutputWriter.WriteMessageOnNewLine("Data read");
+        }
+    }
+}
