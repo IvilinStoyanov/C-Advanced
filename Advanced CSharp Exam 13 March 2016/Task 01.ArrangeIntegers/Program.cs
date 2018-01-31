78021845fb43fde95bf097c86e7e28351ed7d92a
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-01-31 14:20:17 +0200

    Adding Task 01.ArrangeIntegers & Task 04.ChampionsLeague

---
+++ Advanced CSharp Exam 13 March 2016/Task 01.ArrangeIntegers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_01.ArrangeIntegers
+{
+    class Program
+    {
+
+        private static readonly string[] IntegerNames =
+                                    { "zero", "one", "two",
+                                    "three", "four", "five", "six",
+                                    "seven", "eight", "nine" };
+
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+           var orderedInput = input.OrderBy(str => string.Join(string.Empty, str.Select(ch => IntegerNames[ch - '0'])));
+
+            Console.WriteLine(string.Join(", ", orderedInput));
+        }
+    }
+}
+
