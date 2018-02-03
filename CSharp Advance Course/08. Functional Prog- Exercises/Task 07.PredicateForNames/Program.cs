b1665c317a75e3b415a9aec74366d94ade1aec67
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-02-03 13:50:24 +0200

    Add task till 08

---
+++ CSharp Advance Course/08. Functional Prog- Exercises/Task 07.PredicateForNames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameLenght = int.Parse(Console.ReadLine());
+            
+            Predicate<string> IsNameCorrect = name => name.Length <= nameLenght;
+            var names = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(n => IsNameCorrect(n))
+                .ToArray();
+
+            Console.WriteLine(string.Join("\n", names));
+        }
+    }
+}
---
+++ CSharp Advance Course/08. Functional Prog- Exercises/Task 08.CustomComparator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_08.CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var evenNumber = numbers.Where(x => x % 2 == 0).OrderBy(x => x).ToList();
+            var oddNumber = numbers.Where(x => x % 2 != 0).OrderBy(x => x).ToList();
+
+            evenNumber.AddRange(oddNumber);
+
+            Console.WriteLine(string.Join(" ", evenNumber));
+
+        }
+    }
+}
