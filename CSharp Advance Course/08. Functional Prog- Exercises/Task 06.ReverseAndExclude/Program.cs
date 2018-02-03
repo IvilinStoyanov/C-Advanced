e567e52307cc69bb2c6342ef18ca749b023f5384
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-02-03 13:16:50 +0200

    Add Task 06

---
+++ CSharp Advance Course/08. Functional Prog- Exercises/Task 06.ReverseAndExclude/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = Console.ReadLine()
+                .Split(new[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var n = int.Parse(Console.ReadLine());
+
+            Predicate<int> isDivisible = (nn) => nn % n == 0;
+
+            Func<List<int>, int, List<int>> reverseAndExclude = (nums, x) =>
+            {
+                nums.Reverse();
+                nums = nums.Where(y => !isDivisible(y)).ToList();
+                return nums;
+            };
+
+            Console.WriteLine(string.Join(" ", reverseAndExclude(numbers, n)));
+        }
+    }
+}
