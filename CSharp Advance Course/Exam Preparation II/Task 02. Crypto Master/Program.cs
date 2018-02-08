0e786daf005e4947e5166f2441d1d4041c5003b0
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-02-08 15:25:00 +0200

    Adding Task 02 && Task 03

---
+++ CSharp Advance Course/Exam Preparation II/Task 02. Crypto Master/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_02._Crypto_Master
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNumbers = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int maxLenght = 0;
+            int currentSteps = 0;
+
+            for (int steps = 1; steps < inputNumbers.Length; steps++)
+            {
+                for (int startIndex = 0; startIndex < inputNumbers.Length; startIndex++)
+                {
+                    currentSteps = 1;
+
+                    int currentIndex = startIndex;
+                    int nextStep = (currentIndex + steps) % inputNumbers.Length;
+
+                    while (inputNumbers[currentIndex] < inputNumbers[nextStep])
+                    {
+                        currentSteps++;
+
+                        currentIndex = nextStep;
+                        nextStep = (currentIndex + steps) % inputNumbers.Length;
+                    }
+
+                    if (currentSteps > maxLenght)
+                    {
+                        maxLenght = currentSteps;
+                    }
+                }
+            }
+            Console.WriteLine(maxLenght);
+        }
+    }
+}
---
+++ CSharp Advance Course/Exam Preparation II/Task 03. GreedyTimes V2/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_03._GreedyTimes_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long bagCapacity = long.Parse(Console.ReadLine());
+
+            var sequenceOfItemQuantity = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var bagWithDifferentValues = new Dictionary<string, Dictionary<string, long>>();
+
+            long goldTotalValue = 0;
+            long gemTotalvalue = 0;
+            long cashTotalValue = 0;
+
+            long currentBagCapacity = 0;
+
+            for (int i = 0; i < sequenceOfItemQuantity.Length; i += 2)
+            {
+                string typeOfValue = sequenceOfItemQuantity[i];
+                long value = long.Parse(sequenceOfItemQuantity[i + 1]);
+
+                if (bagCapacity >= currentBagCapacity + value)
+                {
+                    if (typeOfValue == "Gold")
+                    {
+                        goldTotalValue += value;
+
+                        if (!bagWithDifferentValues.ContainsKey("Gold"))
+                        {
+                            bagWithDifferentValues.Add("Gold", new Dictionary<string, long>());
+                        }
+                        if (!bagWithDifferentValues["Gold"].ContainsKey(typeOfValue))
+                        {
+                            bagWithDifferentValues["Gold"].Add(typeOfValue, 0);
+                        }
+                        bagWithDifferentValues["Gold"][typeOfValue] += value;
+
+                        currentBagCapacity += value;
+                    }
+
+                    if (typeOfValue.ToLower().EndsWith("gem") && sequenceOfItemQuantity.Length >= 4)
+                    {
+                        if (goldTotalValue >= (gemTotalvalue + value))
+                        {
+                            gemTotalvalue += value;
+
+                            if (!bagWithDifferentValues.ContainsKey("Gem"))
+                            {
+                                bagWithDifferentValues.Add("Gem", new Dictionary<string, long>());
+                            }
+                            if (!bagWithDifferentValues["Gem"].ContainsKey(typeOfValue))
+                            {
+                                bagWithDifferentValues["Gem"].Add(typeOfValue, 0);
+                            }
+                            bagWithDifferentValues["Gem"][typeOfValue] += value;
+
+                            currentBagCapacity += value;
+                        }
+                    }
+
+                    if (typeOfValue.Length == 3)
+                    {
+                        if (gemTotalvalue >= (cashTotalValue + value))
+                        {
+                            cashTotalValue += value;
+
+                            if (!bagWithDifferentValues.ContainsKey("Cash"))
+                            {
+                                bagWithDifferentValues.Add("Cash", new Dictionary<string, long>());
+                            }
+                            if (!bagWithDifferentValues["Cash"].ContainsKey(typeOfValue))
+                            {
+                                bagWithDifferentValues["Cash"].Add(typeOfValue, 0);
+                            }
+                            bagWithDifferentValues["Cash"][typeOfValue] += value;
+
+                            currentBagCapacity += value;
+                        }
+                    }
+                }
+            }
+
+            foreach (var bag in bagWithDifferentValues.OrderByDescending(v => v.Value.Values.Sum()))
+            {
+                Console.WriteLine($"<{bag.Key}> ${bag.Value.Values.Sum()}");
+
+                foreach (var values in bag.Value.OrderByDescending(n => n.Key).ThenBy(v => v.Value))
+                {
+                    Console.WriteLine($"##{values.Key} - {values.Value}");
+                }
+            }
+        }
+    }
+}
