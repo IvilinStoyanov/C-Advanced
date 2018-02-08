ebf32356922a7e76d5cce97766797ed36f9e6baf
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-02-08 21:33:41 +0200

    Adding task 04

---
+++ CSharp Advance Course/Exam Preparation II/Task 04. TreasureMap/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Treasure_Map
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"[#|!][^#!]*?(?<![A-Za-z0-9])(?<streetName>[A-Za-z]{4})(?![A-Za-z0-9])[^#!]*(?<!\d)(?<streetNumber>\d{3})-(?<password>\d{4}|\d{6})(?!\d)[^#!]*?[!|#]";
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+
+                MatchCollection matchCollection = Regex.Matches(input, pattern);
+
+                var correctMatch = matchCollection[matchCollection.Count / 2];
+
+                string streetName = correctMatch.Groups["streetName"].Value;
+                string streetNumber = correctMatch.Groups["streetNumber"].Value;
+                string password = correctMatch.Groups["password"].Value;
+
+                Console.WriteLine($"Go to str. {streetName} {streetNumber}. Secret pass: {password}.");
+            }
+        }
+    }
+}
+
