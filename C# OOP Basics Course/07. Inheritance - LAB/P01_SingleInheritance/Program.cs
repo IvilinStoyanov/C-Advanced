67947255857d4bdc82784ccb8e68fa90a738ee7c
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-02-23 20:28:30 +0200

    Adding till P05_StackOfStrings

---
+++ C# OOP Basics Course/07. Inheritance - LAB/P01_SingleInheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var dog = new Dog();
+        dog.Eat();
+        dog.Bark();
+
+    }
+}
+
---
+++ C# OOP Basics Course/07. Inheritance - LAB/P05_StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class StackOfStrings
+{
+    private string newElement;
+
+    List<string> data = new List<string>();
+
+    public bool isEmpty()
+    {
+      if(data.Count == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void Push(string newElement)
+    {
+         data.Add(newElement);
+    }
+
+    public string Peek()
+    {
+        string result = string.Empty;
+        if(!isEmpty())
+        {
+            result = data[data.Count - 1];
+        }
+        return result;
+    }
+
+    public string Pop()
+    {
+        string result = string.Empty;
+        if (!isEmpty())
+        {
+            var lastIndex = data.Count - 1;
+            result = data[data.Count - 1];
+        }
+        return result;
+    }
+}
+
