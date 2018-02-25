5ec33c31af7ee9b97eef6d106c36237e113230db
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-02-25 19:25:58 +0200

    Adding till P06_Animals(Without P05)

---
+++ C# OOP Basics Course/08. Inheritance - Exercises/P02_BookShop/Book.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Book
+{
+    private string title;
+    private decimal price;
+    private string author;
+
+    public string Title
+    {
+        get { return title; }
+        private set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Title not valid!");
+            }
+            title = value;
+        }
+    }
+
+    public string Author
+    {
+        get { return author; }
+        private set
+        {
+            var indexOfSpace = value.IndexOf(' ');
+            if (indexOfSpace > 0 && indexOfSpace < value.Length - 1 && char.IsDigit(value[indexOfSpace + 1]))
+            {
+                throw new ArgumentException("Author not valid!");
+            }
+            author = value;
+        }
+    }
+
+    protected virtual decimal Price
+    {
+        get { return price; }
+         set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Price not valid!");
+            }
+            price = value;
+        }
+    }
+
+    public Book(string author, string title, decimal price)
+    {
+        this.Author = author;
+        this.Title = title;
+        this.Price = price;
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.Append("Type: ").AppendLine(this.GetType().Name)
+            .Append("Title: ").AppendLine(this.Title)
+            .Append("Author: ").AppendLine(this.Author)
+            .Append("Price: ").Append($"{this.Price:f2}");
+
+        return sb.ToString();
+    }
+}
+
---
+++ C# OOP Basics Course/08. Inheritance - Exercises/P04_OnlineRadioDatabase/Exceptions/InvalidSongNameException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+public class InvalidSongNameException : InvalidSongException
+{
+    public InvalidSongNameException() : base()
+    {
+    }
+
+    public InvalidSongNameException(string message) : base(message)
+    {
+    }
+
+    public InvalidSongNameException(int minNameLength, int maxNameLength)
+        : base($"Song name should be between {minNameLength} and {maxNameLength} symbols.")
+    {
+    }
+}
