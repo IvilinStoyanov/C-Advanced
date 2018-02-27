9640c6768acd5180afc85e7228cc5d2d46a2de6f
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-02-27 14:37:55 +0200

    Add P02_CarsProgram

---
+++ C# OOP Basics Course/09. Interfaces - LAB/P02_CarsProgram/Tesla.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    public class Tesla : IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public int Battery { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("Engine start");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Breaaak!");
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Tesla {this.Model} with {this.Battery} Batteries";
+            
+        }
+    }
+
