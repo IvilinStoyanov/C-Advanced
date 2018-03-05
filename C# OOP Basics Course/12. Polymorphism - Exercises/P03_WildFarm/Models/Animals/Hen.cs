57ee5ec129d06625fee95a7cf24a856730945bbe
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-03-05 15:22:24 +0200

    Adding till P03_WildFarm

---
+++ C# OOP Basics Course/12. Polymorphism - Exercises/P03_WildFarm/Models/Animals/Hen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Hen : Bird
+{
+    public const double IncreaseWeightPercentage = 0.35d;
+
+    public Hen(string name, double weight, double wingSize) : base(name, weight, wingSize)
+    {
+    }
+
+    public override void IncreaseWeight(Food food)
+    {
+        this.Weight += this.FoodEaten * IncreaseWeightPercentage;
+        this.FoodEaten += food.FoodQuantity;
+    }
+
+    public override string ProduceSound()
+    {
+        return "Cluck";
+    }
+
+    public override string ToString()
+    {
+        return $"{this.GetType().Name} [{this.Name}, {this.WingSize}, {this.Weight}, {this.FoodEaten}]";
+    }
+}
+
---
+++ C# OOP Basics Course/12. Polymorphism - Exercises/P03_WildFarm/Models/Animals/Owl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Owl : Bird
+{
+    public const double IncreaseWeightPercentage = 0.25d;
+
+    public Owl(string name, double weight, double wingSize) : base(name, weight, wingSize)
+    {
+
+    }
+
+    public override void IncreaseWeight(Food food)
+    {
+        if (food.GetType().Name == "Meat")
+        {
+            this.Weight += food.FoodQuantity * IncreaseWeightPercentage;
+            this.FoodEaten += food.FoodQuantity;
+        }
+    }
+
+    public override string ProduceSound()
+    {
+        return "Hoot Hoot";
+    }
+
+    public override string ToString()
+    {
+        return $"{this.GetType().Name} [{this.Name}, {this.WingSize}, {this.Weight}, {this.FoodEaten}]";
+    }
+}
+
