05c637cac49bcf0a701891bc89cf7254864ad018
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-03-08 12:44:25 +0200

    Adding Avatar(till structures only)

---
+++ C# OOP Basics Course/OOP_Basics_Exam_July_2017/Avatar/Core/Engine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Engine
+{
+    internal void Run()
+    {
+        string command = string.Empty;
+
+        while((command = Console.ReadLine()) != "Quit")
+        {
+            var commandArgs = command.Split(' ');
+            ExecuteCommand(commandArgs);
+        }
+    }
+
+    public void ExecuteCommand(string[] commandArgs)
+    {
+
+    }
+}
+
---
+++ C# OOP Basics Course/OOP_Basics_Exam_July_2017/Avatar/Core/NationsBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class NationsBuilder
+{
+    //data structures
+    private Dictionary<string, Bender> benders;
+    private Dictionary<string, Monument> monuments;
+    private Nation nation;
+
+
+    public void AssignBender(List<string> benderArgs)
+    {
+        //TODO: Add some logic here … 
+    }
+    public void AssignMonument(List<string> monumentArgs)
+    {
+        //TODO: Add some logic here … 
+    }
+    public string GetStatus(string nationsType)
+    {
+        return "";
+        //TODO: Add some logic here … 
+    }
+    public void IssueWar(string nationsType)
+    {
+        //TODO: Add some logic here … 
+    }
+    public string GetWarsRecord()
+    {
+        return "";
+        //TODO: Add some logic here … 
+    }
+
+}
+
