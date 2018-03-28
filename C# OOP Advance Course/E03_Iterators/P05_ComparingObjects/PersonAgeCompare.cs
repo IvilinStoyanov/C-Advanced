975f0e54621c7e2a4fea6dcda3af03ffcad7bde1
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-03-28 20:27:57 +0300

    Adding homework from E03_Iterators(till P06)

---
+++ C# OOP Advance Course/E03_Iterators/P05_ComparingObjects/PersonAgeCompare.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class PersonAgeCompare : IComparer<Person>
+{
+    public int Compare(Person x, Person y)
+    {
+        return x.Age - y.Age;
+    }
+}
+
