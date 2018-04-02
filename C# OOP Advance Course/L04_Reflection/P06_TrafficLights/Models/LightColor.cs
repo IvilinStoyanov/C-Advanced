d9940d46406b1a47cae21cabad7ca26798004658
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-04-02 21:41:04 +0300

    Adding P06_TrafficLights

---
+++ C# OOP Advance Course/L04_Reflection/P06_TrafficLights/Models/LightColor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P06_TrafficLights.Models
+{
+    public enum LightColor
+    {
+        Red,
+        Green,
+        Yellow
+    }
+}
---
+++ C# OOP Advance Course/L04_Reflection/P06_TrafficLights/Program.cs
@@ -0,0 +1,13 @@
+﻿using P06_TrafficLights.Core;
+using System;
+
+namespace P06_TrafficLights
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            new Engine().Run();
+        }
+    }
+}
