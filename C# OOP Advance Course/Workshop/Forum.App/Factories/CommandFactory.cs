e8dc5fdc2f6eac8d8c3f2acf48a9f58f3e799e53
IvilinStoyanov <ivilin.stoyanov@gmail.com>
2018-04-15 12:36:45 +0300

    Adding Workshop - Forum_App(till Part03)

---
+++ C# OOP Advance Course/Workshop/Forum.App/Factories/CommandFactory.cs
@@ -0,0 +1,53 @@
+﻿namespace Forum.App.Factories
+{
+    using Contracts;
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class CommandFactory : ICommandFactory
+    {
+        private IServiceProvider serviceProvider;
+
+        public CommandFactory(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public ICommand CreateCommand(string commandName)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            Type commandType = assembly
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == commandName + "Command");
+
+            if (commandType == null)
+            {
+                throw new ArgumentException($"{commandName}Command not found!");
+            }
+            if (!typeof(ICommand).IsAssignableFrom(commandType))
+            {
+                throw new InvalidOperationException($"{commandName}Command is not an ICommand!");
+            }
+
+            ConstructorInfo constructor = commandType
+                .GetConstructors()
+                .First();
+
+            ParameterInfo[] ctroParams = constructor
+                .GetParameters();
+
+            object[] arguments = new object[ctroParams.Length];
+
+            for (int i = 0; i < arguments.Length; i++)
+            {
+                arguments[i] = this.serviceProvider.GetService(ctroParams[i].ParameterType);
+            }
+
+            ICommand command = (ICommand)Activator.CreateInstance(commandType, arguments);
+            return command;
+           // ICommand command1 = (ICommand)constructor.Invoke(arguments);
+        }
+    }
+}
---
+++ C# OOP Advance Course/Workshop/Forum.App/Factories/MenyFactory.cs
@@ -0,0 +1,50 @@
+﻿using Forum.App.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Forum.App.Factories
+{
+    public class MenyFactory : IMenuFactory
+    {
+        private IServiceProvider serviceProvider;
+
+        public MenyFactory(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public IMenu CreateMenu(string menuName)
+        {
+            Type menuType = Assembly
+                .GetExecutingAssembly()
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == menuName + "Menu");
+
+            if(menuType == null)
+            {
+                throw new ArgumentException($"{menuName}Menu is not found!");
+            }
+
+            if(!typeof(IMenu).IsAssignableFrom(menuType))
+            {
+                throw new InvalidOperationException($"{menuName}Menu is not an IMenu!");
+            }
+
+            ParameterInfo[] ctroParams = menuType
+                .GetConstructors()
+                .First()
+                .GetParameters();
+
+            object[] arguments = new object[ctroParams.Length];
+
+            for (int i = 0; i < arguments.Length; i++)
+            {
+                arguments[i] = this.serviceProvider.GetService(ctroParams[i].ParameterType);
+            }
+
+            IMenu menu = (IMenu)Activator.CreateInstance(menuType, arguments);
+            return menu;
+        }
+    }
+}
